4593976537dcac23d50b687a2beb1312dd20ac46
sggv950 <38014323+sggv950@users.noreply.github.com>
2019-12-22 15:41:59 +0200

    fixing some sec issues

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,7 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f https://github.com/sggv950/vue-weather-app.git master:gh-pages
+# git push -f https://github.com/sggv950/vue-weather-app.git master:gh-pages
+git push -f git@github.com:sggv950/vue-weather-app.git master:gh-pages
 
 cd -
